a2ef1233669235c56045b3703a2f94c7aac84ad5
ozanpekmezci <ozanpekmezci@gmail.com>
2016-01-12 12:51:14 +0100

    csrf'i duzeltmece 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ before_action :initialize_csrf
   def initialize_omniauth_state
     session['omniauth.state'] = response.headers['X-CSRF-Token'] = form_authenticity_token
   end
-  def initialize_omniauth_state
+  def initialize_csrf
     session[:_csrf_token]   = masked_authenticity_token
   end
 end
