0a5e6d9402c60357107f9502bc650117c7a9b0eb
tuncakagan <tuncakagan@gmail.com>
2016-01-16 15:13:58 +0100

    dhh_tut3

--- app/channels/comments_channel.rb
+++ app/channels/comments_channel.rb
@@ -1,7 +1,8 @@
 class CommentsChannel < ApplicationCable::Channel
   def follow(data)
     stop_all_streams
-    stream_from "posts:#{data['post_id'].to_i}:comments"
+    stream_from 'demo_chan'
+    #stream_from "posts:#{data['post_id'].to_i}:comments"
   end
 
   def unfollow
