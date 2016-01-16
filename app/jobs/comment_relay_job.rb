4a0c6152821fcf5d629427262f7cfc5e91f2c008
tuncakagan <tuncakagan@gmail.com>
2016-01-16 16:36:00 +0100

    empire strikes back

--- app/jobs/comment_relay_job.rb
+++ app/jobs/comment_relay_job.rb
@@ -2,7 +2,7 @@ class CommentRelayJob < ApplicationJob
   queue_as :default
 
   def perform(comment)
-    ActionCable.server.broadcast "posts:#{comment.post_id}:comments",
+    ActionCable.server.broadcast "posts:#{comment.commentable_id}:comments",
       comment: CommentsController.renderer.render(partial: 'comments/comment', locals: { comment: comment })
   end
 end
