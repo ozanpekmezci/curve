63fde929612c38438c2209ff71a3c80995a391c4
tuncakagan <tuncakagan@gmail.com>
2016-01-16 15:43:27 +0100

    dhh_tut5_oldumula

--- app/channels/comments_channel.rb
+++ app/channels/comments_channel.rb
@@ -1,8 +1,8 @@
 class CommentsChannel < ApplicationCable::Channel
   def follow(data)
     stop_all_streams
-    stream_from 'demo_chan'
-    #stream_from "posts:#{data['post_id'].to_i}:comments"
+    #stream_from 'demo_chan'
+    stream_from "posts:#{data['post_id'].to_i}:comments"
   end
 
   def unfollow
@@ -10,7 +10,7 @@ class CommentsChannel < ApplicationCable::Channel
   end
   # subscribed ve unsubscribed otomatik cagiriliyo
   def subscribed
-  	stream_from 'demo_chan'
+  	#stream_from 'demo_chan'
   end
   def speak(data)
   	ActionCable.server.broadcast 'demo_chan', comment: data['comment']
