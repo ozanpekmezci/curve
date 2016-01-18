df3137aabbea755431f91e1c13d263a53c2e45aa
tuncakagan <tuncakagan@gmail.com>
2016-01-18 16:22:03 +0100

    puma_mailer

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-#web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
+web: bundle exec puma -p $PORT /config/puma.rb
 #worker:  bundle exec rake jobs:work
 #web: bundle exec puma -p 5000 ./config.ru
 actioncable: bundle exec puma -p 28080  cable/config.ru
--- config/puma.rb
+++ config/puma.rb
@@ -2,16 +2,16 @@
 http://stackoverflow.com/questions/14842519/unicorn-rb-heroku-delayed-job-config
 for delayed_job
 =end
-worker_processes 3
-preload_app true
-timeout 30
+workers 1
+preload_app!
+# timeout 30
 
 # setting the below code because of the preload_app true setting above:
 # http://unicorn.bogomips.org/Unicorn/Configurator.html#preload_app-method
 
 before_fork do |server, worker|
   if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
+    ActiveRecord::Base.connection_pool.disconnect!
     Rails.logger.info('Disconnected from ActiveRecord')
   end
 end
