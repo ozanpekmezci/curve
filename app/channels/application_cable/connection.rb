2608ded11a7ad7d287ecd74c404b272c46f4e609
ozanpekmezci <ozanpekmezci@gmail.com>
2016-01-20 15:37:19 +0100

    connection.rb kaldirildir

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,22 +1,22 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
+  #  identified_by :current_user
 
-    def connect
-      self.current_user = find_verified_user
-      logger.add_tags 'ActionCable', current_user.name
-    end
+  #  def connect
+  #    self.current_user = find_verified_user
+  #    logger.add_tags 'ActionCable', current_user.name
+  #  end
 
-    protected
-      def find_verified_user
-        #https://github.com/gregmolnar/actioncable-examples/commit/7ae0fd0267a7d6033f5faac92e9ff4cb92fb9714
-        verified_user = User.find_by(id: cookies.signed['user.id'])
-        logger.add_tags 'verified_user', verified_user.id
-           if verified_user && cookies.signed['user.expires_at'] > Time.now
-               verified_user
-        else
-          reject_unauthorized_connection
-        end
-      end
-  end
+#    protected
+#      def find_verified_user
+#        #https://github.com/gregmolnar/actioncable-examples/commit/7ae0fd0267a7d6033f5faac92e9ff4cb92fb9714
+#        verified_user = User.find_by(id: cookies.signed['user.id'])
+#        logger.add_tags 'verified_user', verified_user.id
+#           if verified_user && cookies.signed['user.expires_at'] > Time.now
+#               verified_user
+#        else
+#          reject_unauthorized_connection
+#        end
+#      end
+#  end
 end
