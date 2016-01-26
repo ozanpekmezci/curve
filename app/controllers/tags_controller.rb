e06f1950b7cf23f1f384eb9019f2e4e2eb820320
ozanpekmezci <ozanpekmezci@gmail.com>
2016-01-26 16:36:50 +0100

    actsasnigger9

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -12,8 +12,12 @@ class TagsController < ApplicationController
       @posts = Post.tagged_with(params[:id])
       @current_tag_id = params[:id]
       current_user.follow_tags.push(@current_tag_id)
-      #current_user.update_attributes(follow_tags: ([@current_tag_id].concat(current_user.follow_tags)))
+      respond_to do |format|
       if current_user.save!
+        format.html
+        format.js
+      end
+      #current_user.update_attributes(follow_tags: ([@current_tag_id].concat(current_user.follow_tags)))
         flash[:success] = 'Followed tag'
         redirect_to tag_path(@current_tag_id)
       end
@@ -24,7 +28,11 @@ class TagsController < ApplicationController
       @posts = Post.tagged_with(params[:id])
       @current_tag_id = params[:id]
       current_user.follow_tags.delete(@current_tag_id)
+      respond_to do |format|
       if current_user.save!
+        format.html
+        format.js
+      end
         flash[:error] = 'UNFollowed tag'
         redirect_to tag_path(@current_tag_id)
       end
