158a0aa29c9fea3c79082c043dda40f3e140b615
tuncakagan <tuncakagan@gmail.com>
2016-01-27 15:11:10 +0100

    j(aws)3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,7 @@ Rails.application.configure do
       :bucket => ENV['BUCKET_ID'],
       :access_key_id => ENV['ACCESS_KEY_ID'],
       :secret_access_key => ENV['SECRET_ACCESS_KEY']
-    }
+    },
     :s3_region => :frankfurt
   }
 
