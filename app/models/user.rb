c399ea8991ded098630440c4525d8ed5edc53d5b
ozanpekmezci <ozanpekmezci@gmail.com>
2016-01-31 15:12:20 +0100

    carrierwave8

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,7 @@ class User < ActiveRecord::Base
   ##
   # overrides devise's method and queues the sending of the mail
   def send_devise_notification(notification, *args)
-    devise_mailer.send(notification, self, *args).deliver_later
+  #  devise_mailer.send(notification, self, *args).deliver_later
   end
 
   def follow(other_user)
