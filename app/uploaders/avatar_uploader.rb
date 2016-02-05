c5148a76b6072ea5b67deda766a35c1da4b1678b
tuncakagan <tuncakagan@gmail.com>
2016-02-05 15:13:42 +0100

    custom user edit4

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,7 +15,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
       process resize_to_fill: [280,280]
     end
     version :small_thumb, from_version: :thumb do
-       process resize_to_fill: [20, 20]
+       process resize_to_fill: [75, 75]
      end
 
 
