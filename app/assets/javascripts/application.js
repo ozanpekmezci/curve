a4b3ad589ee73cce6799f496602383345923ecb9
tuncakagan <tuncakagan@gmail.com>
2016-03-01 12:23:41 +0100

    mailboxer6

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,6 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap
-//= require_tree .
 //= require registrations
 // require turbolinks
 //= require jquery.infinitescroll
