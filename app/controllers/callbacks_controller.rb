b03c82f6a2262729cba9afbd6afefd1f058397c5
ozanpekmezci <ozanpekmezci@gmail.com>
2016-03-17 12:45:29 +0200

    omniauth-facebook7

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -7,7 +7,6 @@ class CallbacksController < Devise::OmniauthCallbacksController
     def facebook
      # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env["omniauth.auth"])
-      flash.now[request.env["omniauth.auth"]]
 
     if @user.persisted?
        sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
