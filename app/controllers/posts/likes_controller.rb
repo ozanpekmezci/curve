5e0b285e4161dcdf2062cc6f677708f042951c5d
ozanpekmezci <ozanpekmezci@gmail.com>
2016-03-18 17:13:11 +0200

    post_like2

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -1,6 +1,6 @@
 class Posts::LikesController < ApplicationController
-  before_action: :authenticate_user!
-  before_action: :set_post
+  before_action :authenticate_user!
+  before_action :set_post
   def create
     @post.likes.where(user_id: current_user.id).first_or_create
     respond_to do |format|
