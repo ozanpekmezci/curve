cd2e08597fa44f38decf5cec8eb403b48d96464a
Kagan Tunca <tuncakagan@gmail.com>
2016-03-31 17:08:50 +0200

    actioncable oldurmaca8

--- app/jobs/post_relay_job.rb
+++ app/jobs/post_relay_job.rb
@@ -1,7 +1,7 @@
 class PostRelayJob < ApplicationJob
   queue_as :default
 
-  def perform(post)
+  def perform(post, current_user)
     ActionCable.server.broadcast "posts",
       post: PostsController.renderer.render(partial: 'posts/post', locals: { post: post, user: current_user })
   end
