7a322ee93b6632e0757b0cca79dd63a0a8a402f9
Kagan Tunca <tuncakagan@gmail.com>
2016-04-01 13:10:31 +0200

    yeni gün yeni actioncable

--- app/jobs/post_relay_job.rb
+++ app/jobs/post_relay_job.rb
@@ -2,8 +2,25 @@ class PostRelayJob < ApplicationJob
   queue_as :default
 
   def perform(post)
-    ActionCable.server.broadcast "posts",
-      post: PostsController.renderer.render(partial: 'posts/post', locals: { post: post})
-  end
 
+    @following_users = []
+    # @following_tags = []
+
+    User.find_by(id: post.user_id).followers.each do |u|
+        @following_users.push(u)
+    end
+
+    User.all.each do |u|
+      if (u.follow_tags & post.all_labels_list).any?
+        @following_users.push(u)
+      end
+    end
+
+    @following_users.uniq!
+
+    @following_users.each do |u|
+      ActionCable.server.broadcast "posts_#{u.id}",
+        post: PostsController.renderer.render(partial: 'posts/post', locals: { post: post, user: u})
+    end
+  end
 end
