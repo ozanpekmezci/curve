3bc38b4d0555816a91ea5d9632248e052347e124
Kagan Tunca <tuncakagan@gmail.com>
2016-04-01 14:17:03 +0200

    yeni gün yeni actioncable5

--- app/channels/posts_channel.rb
+++ app/channels/posts_channel.rb
@@ -2,7 +2,7 @@ class PostsChannel < ApplicationCable::Channel
   def follow(current_user_id)
     stop_all_streams
     #stream_from 'demo_chan'
-    stream_from "posts_#{current_user_id}"
+    stream_from "posts_#{current_user_id['current_user_id'].to_i}"
   end
 
   def unfollow
