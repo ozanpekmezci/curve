877294ed4d0abab675120fa8973658c34fbbb282
ozanpekmezci <ozanpekmezci@gmail.com>
2016-04-10 15:51:13 +0200

    commente pundit ve render index4

--- app/jobs/comment_relay_job.rb
+++ app/jobs/comment_relay_job.rb
@@ -2,7 +2,7 @@ class CommentRelayJob < ApplicationJob
   queue_as :default
 
   def perform(comment)
-    ActionCable.server.broadcast "posts:#{comment.commentable_id}:comments", {user_id: comment.user_id.to_i}
+    ActionCable.server.broadcast "posts:#{comment.commentable_id}:comments", {user_id: "#{comment.user_id}"}
     #  comment: CommentsController.renderer.render(partial: 'comments/comment', locals: { comment: comment })
   end
 end
