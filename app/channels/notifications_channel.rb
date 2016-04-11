68fe103e2c1814ea648a5439c47dfed10c6dcbda
ozanpekmezci <ozanpekmezci@gmail.com>
2016-04-11 18:25:17 +0200

    notif actioncable denemeler8

--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -1,6 +1,7 @@
 class NotificationsChannel < ApplicationCable::Channel
   def follow(data)
      #stream_from 'demo_chan'
+    stop_all_streams
     stream_from "notifications_#{data['current_user_id'].to_i}"
   end
 
