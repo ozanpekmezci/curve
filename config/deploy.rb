63bb7f7d7922a8ef212b960e1460f9ed96857ca9
ozanpekmezci <ozanpekmezci@gmail.com>
2016-05-02 23:24:54 +0200

    foreman test6

--- config/deploy.rb
+++ config/deploy.rb
@@ -87,7 +87,7 @@ namespace :foreman do
   desc "Export the Procfile to Ubuntu's upstart scripts"
   task :export do
       on roles(:app) do
-    run "cd #{current_path} &&  foreman export upstart /etc/init -a #{fetch(:application)} -u #{fetch(:application)} -l /var/#{fetch(:application)}/log"
+    run "cd #{current_path} && foreman export upstart -a #{fetch(:application)} -u #{fetch(:application)} -l /var/#{fetch(:application)}/log /etc/init" 
   end
   end
 
