cb53e456721e55cb0a1d1a9b452bec1db0b768ad
Kagan Tunca <tuncakagan@gmail.com>
2016-08-08 17:56:33 +0200

    fire_form reactxii

--- app/assets/javascripts/components/fire_form.js.jsx
+++ app/assets/javascripts/components/fire_form.js.jsx
@@ -15,7 +15,7 @@ var FireForm = React.createClass({
   },
 
   handleSubmit: function(e) {
-    let path = {"chats/"+this.props.chat.id+"/fires"};
+    let path = "chats/"+this.props.chat.id+"/fires";
     e.preventDefault();
     $.post( path,
            { fire: this.state },
