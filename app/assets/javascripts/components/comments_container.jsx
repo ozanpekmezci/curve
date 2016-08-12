e5ad1cf2a23bde348d8dbedf2f1f95678d999e25
ozanpekmezci <ozanpekmezci@gmail.com>
2016-08-12 15:50:58 +0300

    onclick show/hide done(hopefully)

---
+++ app/assets/javascripts/components/comments_container.jsx
@@ -0,0 +1,26 @@
+var CommentsContainer = React.createClass({
+  getInitialState: function() {
+         return { showForm: false };
+     },
+     onClick: function() {
+         this.setState({ showForm: !this.state.showForm });
+     },
+
+
+  render: function() {
+    return (
+  <div>
+    <input type="submit" value="Supply" className='btn btn-success' onClick={this.onClick} />
+                { this.state.showForm ?   <CommentForm
+                        post={this.props.postobj}
+                        path={this.props.path}
+                      /> : null }
+
+        <Comments
+          post={this.props.postview}
+          path={this.props.path}
+        />
+  </div>
+);
+  }
+});
