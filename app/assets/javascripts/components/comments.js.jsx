e8beae21a008ee1297f4ad99157197ad61b56596
ozanpekmezci <ozanpekmezci@gmail.com>
2016-08-19 18:56:45 +0300

    comment devam cap'ta baska error fix4

--- app/assets/javascripts/components/comments.js.jsx
+++ app/assets/javascripts/components/comments.js.jsx
@@ -21,8 +21,8 @@ var Comments = React.createClass({
 
     <section id="comments" data-channel="comments" data-post-id={this.props.post.id}>
      {this.state.post.comments.map(function(comment,index) {
-
-     return   {index<length-showSupplyAmount?<Comment key={comment.id} comment={comment} path={path}  />: null}
+        if
+     return  ( {index<length-showSupplyAmount?<Comment key={comment.id} comment={comment} path={path}  />: null});
    })}
    </section>
 </div>);
