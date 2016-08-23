a7b6ebaa00739782e8ac8fa89ecb46edbd1f71a1
ozanpekmezci <ozanpekmezci@gmail.com>
2016-08-23 10:35:34 +0300

    react pundit1

---
+++ app/assets/javascripts/policies/comment_policy.js
@@ -0,0 +1,31 @@
+/*
+ * Policy object, returning a single `permit` action
+ *
+ * @param action [String]
+ * @param record [Object]
+ *
+ * @example
+ *   if (CommentPolicy.permit('update', record)) { ... }
+ *
+ * @return [Boolean]
+ */
+var CommentPolicy = (function(currentUser, undefined) {
+
+  var _policies = {
+    update: function(record) {
+      return currentUser.id === record.user_id;
+    },
+    destroy: function(record) {
+      return this.update(record) || currentUser.role === 'admin';
+    },
+  };
+
+  var permit = function(action, record) {
+    return _policies[action](record);
+  }
+
+  return {
+    permit: permit
+  };
+
+})(window.currentUser)
