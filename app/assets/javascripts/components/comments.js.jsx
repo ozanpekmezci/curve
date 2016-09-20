9557bcf88fd34cf90e8f86b0b6ca7ada4a9c4653
ozanpekmezci <ozanpekmezci@gmail.com>
2016-09-20 09:48:08 +0300

    edit calismiyor commentte - done , controllerda render json olmadigi icin succes'e data gelmiyormus

--- app/assets/javascripts/components/comments.js.jsx
+++ app/assets/javascripts/components/comments.js.jsx
@@ -3,94 +3,103 @@ var Comments = React.createClass({
     path: React.PropTypes.string
   },
   getInitialState: function() {
-     let post = JSON.parse(this.props.post);
-     return { post: post,user_id:this.props.current_user_id, showSupplyAmount: false,supplyAmount:0 };
-   },
+    let post = JSON.parse(this.props.post);
+    return { post: post,user_id:this.props.current_user_id, showSupplyAmount: false,supplyAmount:0 };
+  },
 
 
-   getDefaultProps: function() {
-     return { comments: [] };
-   },
-    deleteRecord: function(comment) {
+  getDefaultProps: function() {
+    return { comments: [] };
+  },
+  deleteRecord: function(comment) {
     var index = this.state.post.comments.indexOf(comment);
     var comments = React.addons.update(this.state.post.comments,
-                                      { $splice: [[index, 1]] });
-    let post = this.state.post;
-    post.comments = comments
-    this.setState({ post: post,showSupplyAmount:false,supplyAmount:0 });
-  },
-  updateRecord: function(comment, data) {
-  console.log("update");
-console.log(comment);
-console.log(data);
-    var index = this.state.post.comments.indexOf(comment);
-  var comments = React.addons.update(this.state.post.comments,
-                                    { $splice: [[index, 1, data]] });
-  let post = this.state.post;
-  post.comments = comments
-  console.log("post"+post.comments.length);
-this.setState({ post: post,showSupplyAmount:false,supplyAmount:0 });
-console.log("setstate");
-  },
-
-
-   render: function() {
-    let length=this.state.post.comments.length;
-    let supplyAmount= this.state.supplyAmount;
-    let path=this.props.path;
-    let post = this.state.post;
-    console.log(this.state.post.comments);
-
-     return(
-    <div>{ this.state.showSupplyAmount ? <div onClick={this.handleClick}> {this.state.supplyAmount} new supply </div> : null }
-
-    <section id="comments" data-channel="comments" data-post-id={post.id}>
-     {this.state.post.comments.map(function(comment,index) {
-      console.log("comment"+index);
-      console.log(comment);
-
-     return  ( <div>{index<length-supplyAmount?<Comment key={comment.id} comment={comment} path={path} post={post}  handleDeleteRecord={this.deleteRecord}
-        handleEditRecord={this.updateRecord}/>: null}</div>);
-   }.bind(this))}
-   </section>
-</div>);
-
+      { $splice: [[index, 1]] });
+      let post = this.state.post;
+      post.comments = comments
+      this.setState({ post: post,showSupplyAmount:false,supplyAmount:0 });
     },
-    componentDidMount: function() {
-      this.setupSubscription();
-    },
-
-    updatePost: function(comment) {
-      let post = JSON.parse(comment);
-      this.setState({ post: post });
-    },
-    handleReceived: function() {
-       this.setState({ showSupplyAmount: true,supplyAmount: (this.state.supplyAmount + 1)  });
-   },
-   handleClick: function() {
-      this.setState({ showSupplyAmount: false,supplyAmount: this.getInitialState().supplyAmount });
-
-  },
+    updateRecord: function(comment, data) {
+      var index = this.state.post.comments.indexOf(comment);
+      var comments = React.addons.update(this.state.post.comments,
+        { $splice: [[index, 1, data]] });
+        let post = this.state.post;
+        post.comments = comments
+        this.setState({ post: post,showSupplyAmount:false,supplyAmount:0 });
+      },
 
 
-    setupSubscription: function() {
+      render: function() {
+        let length=this.state.post.comments.length;
+        let supplyAmount= this.state.supplyAmount;
+        let path=this.props.path;
+        let post = this.state.post;
+        return(
+          <div>
+            { this.state.showSupplyAmount ?
+              <div onClick={this.handleClick}> {this.state.supplyAmount} new supply </div>
+              : null }
 
-      App.comments = App.cable.subscriptions.create("CommentsChannel", {
-        post_id: this.state.post.id,
-        user_id: this.state.user_id,
-        showSupplyAmount: this.state.showSupplyAmount,
-        supplyAmount: this.state.supplyAmount,
-        connected: function () {
-          setTimeout(() => this.perform('follow', { post_id: this.post_id }), 1000);
-        },
-        userIsCurrentUser: function(user_id) {
+              <section
+                id="comments"
+                data-channel="comments"
+                data-post-id={post.id}>
+                {this.state.post.comments.map(function(comment,index) {
 
-          return(user_id==this.user_id);
+                  return  (
+                    <div>
+                      {index<length-supplyAmount?
+                        <Comment
+                          key={comment.id}
+                          comment={comment}
+                          path={path}
+                          post={post}
+                          handleDeleteRecord={this.deleteRecord}
+                          handleEditRecord={this.updateRecord}/>
+                        : null}
+                      </div>
+                    );
+                  }.bind(this))}
+                </section>
+              </div>
+            );
 
-        },
-        received: function (data) {
-         if(!this.userIsCurrentUser(data.user_id)){
-          this.handleReceived();
+          },
+          componentDidMount: function() {
+            this.setupSubscription();
+          },
+
+          updatePost: function(comment) {
+            let post = JSON.parse(comment);
+            this.setState({ post: post });
+          },
+          handleReceived: function() {
+            this.setState({ showSupplyAmount: true,supplyAmount: (this.state.supplyAmount + 1)  });
+          },
+          handleClick: function() {
+            this.setState({ showSupplyAmount: false,supplyAmount: this.getInitialState().supplyAmount });
+
+          },
+
+
+          setupSubscription: function() {
+
+            App.comments = App.cable.subscriptions.create("CommentsChannel", {
+              post_id: this.state.post.id,
+              user_id: this.state.user_id,
+              showSupplyAmount: this.state.showSupplyAmount,
+              supplyAmount: this.state.supplyAmount,
+              connected: function () {
+                setTimeout(() => this.perform('follow', { post_id: this.post_id }), 1000);
+              },
+              userIsCurrentUser: function(user_id) {
+
+                return(user_id==this.user_id);
+
+              },
+              received: function (data) {
+                if(!this.userIsCurrentUser(data.user_id)){
+                  this.handleReceived();
 
 
 
@@ -102,16 +111,16 @@ console.log("setstate");
 
 
 
-          }else{
+                }else{
 
 
-        }
-          this.updatePost(data.comment);
-        },
+                }
+                this.updatePost(data.comment);
+              },
 
-        updatePost: this.updatePost.bind(this),
-        handleReceived: this.handleReceived.bind()
+              updatePost: this.updatePost.bind(this),
+              handleReceived: this.handleReceived.bind()
 
-      });
-    }
-});
+            });
+          }
+        });
